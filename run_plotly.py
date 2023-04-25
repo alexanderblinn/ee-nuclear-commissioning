e3875c10804ea1c0ec703a1eea13e3f90c0d08af
Alexander Blinn <alexander.blinn@outlook.de>
2023-04-25 20:07:48 +0200

    Update file

--- run_plotly.py
+++ run_plotly.py
@@ -15,9 +15,6 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'config'))
-plt.style.use(os.path.join(ROOT, 'mplstyle-ibt'))
-
 # %%
 
 YEAR_START = 1990
@@ -28,51 +25,66 @@ YEAR_END = 2023
 # Define a list of colors
 COUNTRY_COLORS = {
     "Belarus": "#0072b1",
-    "Belgien": "#e6a000",
-    "Bulgarien": "#009e73",
-    "Deutschland": "#d45e00",
-    "Finnland": "#9300d3",
-    "Frankreich": "#fac200",
-    "Italien": "#5c5c5c",
-    "Litauen": "#e63227",
-    "Niederlande": "#9e9e00",
-    "Österreich": "#00bcd4",
-    "Polen": "#e68200",
-    "Rumänien": "#6b6b6b",
-    "Schweden": "#2ca02c",
-    "Schweiz": "#a172de",
-    "Slowakei": "#ff9900",
-    "Slowenien": "#008080",
-    "Spanien": "#c0c0c0",
-    "Tschechien": "#c4022b",
-    "Türkei": "#4b0082",
+    "Belgium": "#e6a000",
+    "Bulgaria": "#009e73",
+    "Germany": "#d45e00",
+    "Finland": "#9300d3",
+    "France": "#fac200",
+    "Italy": "#5c5c5c",
+    "Lithuania": "#e63227",
+    "Netherlands": "#9e9e00",
+    "Austria": "#00bcd4",
+    "Poland": "#e68200",
+    "Romania": "#6b6b6b",
+    "Sweden": "#2ca02c",
+    "Switzerland": "#a172de",
+    "Slovakia": "#ff9900",
+    "Slovenia": "#008080",
+    "Spain": "#c0c0c0",
+    "Czech Republic": "#c4022b",
+    "Turkey": "#4b0082",
     "Ukraine": "#800080",
-    "Ungarn": "#0000ff",
-    "Verinigtes Königreich": "#ff0000"
+    "Hungary": "#0000ff",
+    "United Kingdom": "#ff0000"
 }
 
 def calculate_operational_time(row):
     if row["Status"] == "In Betrieb":
-        return (datetime.now() - row["Kommerzieller Betrieb (geplant)"]).total_seconds() / 86400 / 365.25
+        return (datetime.now() - row["Kommerzieller Betrieb"]).total_seconds() / 86400 / 365.25
     else:
         try:
-            return (row["Abschaltung (geplant)"] - row["Kommerzieller Betrieb (geplant)"]).total_seconds() / 86400 / 365.25
+            return (row["Abschaltung"] - row["Kommerzieller Betrieb"]).total_seconds() / 86400 / 365.25
         except:
             return None
 
 # %%
 
-df = pd.read_excel("nuclear_power_plants.xlsx")
+
+
+FILE_NAME = "nuclear_power_plants.xlsx"
+
+FILE_PATH = os.path.join(os.path.dirname(__file__), "data", FILE_NAME)
+
+df = pd.read_excel(
+    FILE_PATH,
+    converters= {
+        "Baubeginn": pd.to_datetime,
+        "erste Netzsynchronisation": pd.to_datetime,
+        "Kommerzieller Betrieb": pd.to_datetime,
+        "Abschaltung": pd.to_datetime,
+        "Bau/Projekt eingestellt": pd.to_datetime,
+        }
+    )
 
 df["Jahr_Baubeginn"] = df["Baubeginn"].apply(
     lambda x: x.year if isinstance(x, datetime) else None
     )
 
-df["Jahr_Inbetriebnahme"] = df["Kommerzieller Betrieb (geplant)"].apply(
+df["Jahr_Inbetriebnahme"] = df["Kommerzieller Betrieb"].apply(
     lambda x: x.year if isinstance(x, datetime) else None
     )
 
-df["Jahr_Abschaltung"] = df["Abschaltung (geplant)"].apply(
+df["Jahr_Abschaltung"] = df["Abschaltung"].apply(
     lambda x: x.year if isinstance(x, datetime) else None
     )
 
@@ -134,107 +146,109 @@ MAX_POWER = data["Leistung, Netto in MW"].max()
 for country in countries:
     # In-operation reactors for the current country
     country_data = data.loc[(data["Status"]=="In Betrieb") & (data["Land"]==country)]
+    if not country_data.empty:
 
-    x1 = country_data["Kommerzieller Betrieb (geplant)"]
-    y1 = country_data["Betriebszeit"]
-    s1 = country_data["size"]
-    c1 = country_data["color"]
+        x1 = country_data["Kommerzieller Betrieb"]
+        y1 = country_data["Betriebszeit"]
+        s1 = country_data["size"]
+        c1 = country_data["color"]
 
-    # Land
-    t1 = country_data["Land"]
-    t1 += "<br>"
-    # Name und Block
-    t1 += country_data["Name"]
-    t1 += ", Block "
-    t1 += country_data["Block"].apply(str)
-    t1 += "<br>"
-    # Inbetriebnahme
-    t1 += "Inbetriebnahme: "
-    t1 += country_data["Kommerzieller Betrieb (geplant)"].apply(lambda x: x.strftime("%B %Y"))
-    t1 += "<br>"
-    # Alter
-    t1 += "derzeitiges Alter: "
-    t1 += country_data["Betriebszeit"].round(2).apply(str)
-    t1 += " Jahre"
-    t1 += "<br>"
-    # Leistung
-    t1 += "Nettoleistung: "
-    t1 += country_data["Leistung, Netto in MW"].apply(str)
-    t1 += " MW"
+        # Land
+        t1 = country_data["Land"]
+        t1 += "<br>"
+        # Name und Block
+        t1 += country_data["Name"]
+        t1 += ", Block "
+        t1 += country_data["Block"].apply(str)
+        t1 += "<br>"
+        # Inbetriebnahme
+        t1 += "Inbetriebnahme: "
+        t1 += country_data["Kommerzieller Betrieb"].apply(lambda x: x.strftime("%B %Y"))
+        t1 += "<br>"
+        # Alter
+        t1 += "derzeitiges Alter: "
+        t1 += country_data["Betriebszeit"].round(2).apply(str)
+        t1 += " Jahre"
+        t1 += "<br>"
+        # Leistung
+        t1 += "Nettoleistung: "
+        t1 += country_data["Leistung, Netto in MW"].apply(str)
+        t1 += " MW"
 
-    fig.add_trace(
-        go.Scatter(
-            x=x1,
-            y=y1,
-            mode="markers",
-            marker=dict(size=s1,
-                        color=c1,
-                        colorscale="Viridis",
-                        showscale=False),
-            text=t1,
-            hovertemplate="%{text}<extra></extra>",
-            name=country,
-            legendgroup=country,
-            showlegend=True
+        fig.add_trace(
+            go.Scatter(
+                x=x1,
+                y=y1,
+                mode="markers",
+                marker=dict(size=s1,
+                            color=c1,
+                            colorscale="Viridis",
+                            showscale=False),
+                text=t1,
+                hovertemplate="%{text}<extra></extra>",
+                name=country,
+                legendgroup=country,
+                showlegend=True
+            )
         )
-    )
 
     # Shut down reactors for the current country
     country_data = data.loc[(data["Status"]=="Stillgelegt") & (data["Land"]==country)]
 
-    x2 = country_data["Abschaltung (geplant)"]
-    y2 = country_data["Betriebszeit"]
-    s2 = country_data["size"]
-    c2 = country_data["color"]
+    if not country_data.empty:
+        x2 = country_data["Abschaltung"]
+        y2 = country_data["Betriebszeit"]
+        s2 = country_data["size"]
+        c2 = country_data["color"]
 
-    # Land
-    t2 = country_data["Land"]
-    t2 += "<br>"
-    # Name und Block
-    t2 += country_data["Name"]
-    t2 += ", Block "
-    t2 += country_data["Block"].apply(str)
-    t2 += "<br>"
-    # Inbetriebnahme
-    t2 += "Abschaltung: "
-    t2 += country_data["Abschaltung (geplant)"].apply(lambda x: x.strftime("%B %Y"))
-    t2 += "<br>"
-    # Alter
-    t2 += "Alter bei Abschaltung: "
-    t2 += country_data["Betriebszeit"].round(2).apply(str)
-    t2 += " Jahre"
-    t2 += "<br>"
-    # Leistung
-    t2 += "Nettoleistung: "
-    t2 += country_data["Leistung, Netto in MW"].apply(str)
-    t2 += " MW"
+        # Land
+        t2 = country_data["Land"]
+        t2 += "<br>"
+        # Name und Block
+        t2 += country_data["Name"]
+        t2 += ", Block "
+        t2 += country_data["Block"].apply(str)
+        t2 += "<br>"
+        # Inbetriebnahme
+        t2 += "Abschaltung: "
+        t2 += country_data["Abschaltung"].apply(lambda x: x.strftime("%B %Y"))
+        t2 += "<br>"
+        # Alter
+        t2 += "Alter bei Abschaltung: "
+        t2 += country_data["Betriebszeit"].round(2).apply(str)
+        t2 += " Jahre"
+        t2 += "<br>"
+        # Leistung
+        t2 += "Nettoleistung: "
+        t2 += country_data["Leistung, Netto in MW"].apply(str)
+        t2 += " MW"
 
-    # Check if there are no in-operation reactors for the current country
-    no_in_operation_reactors = len(data.loc[(data["Status"]=="In Betrieb") & (data["Land"]==country)]) == 0
+        # Check if there are no in-operation reactors for the current country
+        no_in_operation_reactors = len(data.loc[(data["Status"]=="In Betrieb") & (data["Land"]==country)]) == 0
 
-    fig.add_trace(
-        go.Scatter(
-            x=x2,
-            y=-y2,
-            mode="markers",
-            marker=dict(size=s2,
-                        color=c2,
-                        colorscale="Viridis",
-                        showscale=False),
-            text=t2,
-            hovertemplate="%{text}<extra></extra>",
-            name=country,
-            legendgroup=country,
-            showlegend=no_in_operation_reactors  # Show legend if there are no in-operation reactors for the current country
+        fig.add_trace(
+            go.Scatter(
+                x=x2,
+                y=-y2,
+                mode="markers",
+                marker=dict(size=s2,
+                            color=c2,
+                            colorscale="Viridis",
+                            showscale=False),
+                text=t2,
+                hovertemplate="%{text}<extra></extra>",
+                name=country,
+                legendgroup=country,
+                showlegend=no_in_operation_reactors  # Show legend if there are no in-operation reactors for the current country
+            )
         )
-    )
 
 
 
 # %%
 
 # Compute the regression line
-x_values = [date.toordinal() for date in data.loc[data["Status"]=="Stillgelegt", "Abschaltung (geplant)"]]
+x_values = [date.toordinal() for date in data.loc[data["Status"]=="Stillgelegt", "Abschaltung"]]
 y_values = data.loc[data["Status"]=="Stillgelegt", "Betriebszeit"]
 regress_coeffs = np.polyfit(x_values, -y_values, deg=1)
 
@@ -275,7 +289,7 @@ fig.update_yaxes(range=[y_min, y_max])
 # Add rectangle
 # fig.add_shape(
 #     type="rect",
-#     x0=data["Kommerzieller Betrieb (geplant)"].sort_values().iloc[len(data["Kommerzieller Betrieb (geplant)"]) -3],
+#     x0=data["Kommerzieller Betrieb"].sort_values().iloc[len(data["Kommerzieller Betrieb"]) -3],
 #     x1=x_max,
 #     y0=0,
 #     y1=y_max,
@@ -288,7 +302,7 @@ fig.update_yaxes(range=[y_min, y_max])
 
 
 fig.add_annotation(
-    text="Inbetriebnahme<br>Abschaltung",
+    text="Commissioning<br>Decommissioning",
     x=datetime(YEAR_START, 1, 1),
     y=0,
     xref="x",
@@ -303,12 +317,12 @@ fig.add_annotation(
 
 fig.add_annotation(
     go.layout.Annotation(
-        text="durchschnittliches Alter bei<br>Abschaltung deutlich gestiegen",
+        text="The average age of nuclear reactors<br>at the time of decommissioning has<br>significantly increased.",
         xref="x",
         yref="y",
         x=datetime(2018, 6, 1),
         y=-40,
-        font=dict(size=14, color="black"),
+        font=dict(size=12, color="black"),
         bordercolor="black",
         borderwidth=1,
         bgcolor="white",
@@ -341,11 +355,11 @@ fig.add_annotation(
 #         method="restyle",
 #         args=[
 #             {"x": [
-#                 data.loc[(data["Status"]=="In Betrieb") & (data["Land"]==country) & (data["Jahr_Inbetriebnahme"] <= i), "Kommerzieller Betrieb (geplant)"]
+#                 data.loc[(data["Status"]=="In Betrieb") & (data["Land"]==country) & (data["Jahr_Inbetriebnahme"] <= i), "Kommerzieller Betrieb"]
 #                 if country in countries else None
 #                 for country in countries
 #             ] + [
-#                 data.loc[(data["Status"]=="Stillgelegt") & (data["Land"]==country) & (data["Jahr_Abschaltung"] <= i), "Abschaltung (geplant)"]
+#                 data.loc[(data["Status"]=="Stillgelegt") & (data["Land"]==country) & (data["Jahr_Abschaltung"] <= i), "Abschaltung"]
 #                 if country in countries else None
 #                 for country in countries
 #             ]}
@@ -365,9 +379,9 @@ fig.add_annotation(
 
 
 fig.update_layout(
-    title="Entwicklung der Atomkraftwerke in Europa seit 1990:<br>Inbetriebnahme und Stilllegung von Reaktoren",
-    xaxis_title="Jahr der Inbetriebnahme bzw. der Abschaltung",
-    yaxis_title="derzeitiges Betriebsalter bzw. Alter bei Abschaltung",
+    title="Evolution of Nuclear Power Plants in Europe since 1990:<br>Commissioning and Decommissioning of Reactors",
+    xaxis_title="Year of Commissioning or Decommissioning",
+    yaxis_title="Current Operational Age or Age at Decommissioning",
     xaxis=dict(showgrid=True, gridwidth=1, gridcolor='rgba(128, 128, 128, 0.1)'),
     yaxis=dict(showgrid=True, gridwidth=1, gridcolor='rgba(128, 128, 128, 0.1)'),
     plot_bgcolor='rgba(0, 0, 0, 0)',
@@ -375,11 +389,11 @@ fig.update_layout(
     # hovermode='x unified',
     hoverlabel=dict(font=dict(size=16)),
     font=dict(family="Roboto", color="black", size=12), # Update this line to set the global font size
-    width=1000,
+    width=997,
     height=580,
     legend=dict(
         # font=dict(color="black"),
-        tracegroupgap=5, # Set the desired padding between legend entries
+        tracegroupgap=4, # Set the desired padding between legend entries
     ),
 )
 
