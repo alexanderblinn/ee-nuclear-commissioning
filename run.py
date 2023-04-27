bc9b2984900ceeeab35960f6a6c596d2033c919a
Alexander Blinn <alexander.blinn@outlook.de>
2023-04-27 13:19:46 +0200

    Update file

--- run.py
+++ run.py
@@ -15,11 +15,12 @@ import plotly.graph_objects as go
 locale.setlocale(locale.LC_TIME, "us_US.UTF-8")
 
 # Constants
-YEAR_START = 1990
+YEAR_START = 1968
 YEAR_END = 2023
 
 # Define a dictionary of colors for each country
 COUNTRY_COLORS = {
+    "Austria": "#FF00FF",
     "Belarus": "#0072b1",
     "Belgium": "#e6a000",
     "Bulgaria": "#009e73",
@@ -27,11 +28,13 @@ COUNTRY_COLORS = {
     "Finland": "#9300d3",
     "France": "#000000",
     "Germany": "#d45e00",
+    "Hungary": "#FFD700",
     "Italy": "#2e4053",
     "Lithuania": "#FA8072",
     "Netherlands": "#9e9e00",
     "Romania": "#a65959",
     "Slovakia": "#36648B",
+    "Slovenia": "#7DF9FF",
     "Spain": "#c0c0c0",
     "Sweden": "#2ca02c",
     "Switzerland": "#a172de",
@@ -77,8 +80,8 @@ def process_data(df: pd.DataFrame) -> pd.DataFrame:
 
 def select_data(df: pd.DataFrame, start: int, end: int) -> pd.DataFrame:
     """Select data within start and end year."""
-    # Drop reactors that never operated.
-    df = df[df["Status"].isin(["In Betrieb", "Stillgelegt"])].dropna(subset=["Betriebszeit"])
+    df.loc[
+        (df["Status"] == "Stillgelegt") & (pd.isna(df["Kommerzieller Betrieb"])), "Betriebszeit"] = 0
     return df.loc[
         (df["Status"] == "In Betrieb")
         &
@@ -112,7 +115,7 @@ data = select_data(df, YEAR_START, YEAR_END)
 data["color"] = data["Land"].map(COUNTRY_COLORS)
 
 # Calculate and scale the size of the bubbles
-data["size"] = 5 + calc_bubble_size(data["Leistung, Netto in MW"]) * (25 - 5)
+data["size"] = 10 + calc_bubble_size(data["Leistung, Netto in MW"]) * 10
 
 # Create a Plotly Figure
 fig = go.Figure()
@@ -126,6 +129,167 @@ fig.add_trace(
         line=dict(color="black", width=1),
         showlegend=False,
         stackgroup="below"
+        )
+    )
+
+# Add rectangle
+year_rec = 2007
+num_up = data.loc[data["Kommerzieller Betrieb"] >= datetime(year_rec, 1, 1), :].shape[0]
+num_down = data.loc[data["Abschaltung"] >= datetime(year_rec, 1, 1), :].shape[0]
+
+msg = """
+Over the course of the last 15 years, the<br>
+European nuclear energy landscape underwent a<br>
+significant transformation. During the years,<br>
+a mere four new reactors commenced operation,<br>
+while a striking number of 42 reactors were<br>
+decommissioned across the continent.<br>
+Meanwhile, the average age of decommissioned<br>
+reactors has surpassed the global average of<br>
+27 years, with a continuing upward trend.<br>
+"""
+
+fig.add_vrect(
+    x0=datetime(2007, 1, 1),
+    x1=datetime(YEAR_END, 12, 31),
+    annotation_text=msg,
+    annotation_position="top left",
+    annotation_align="left",
+    annotation={
+        "font": {"size": 11}
+        },
+    fillcolor="black",
+    opacity=0.1,
+    line_width=0
+    )
+
+fig.add_vline(
+    x=datetime(2007, 1, 1),
+    line_width=1, line_dash="dash", line_color="black"
+    )
+
+# %%
+
+# Define nuclear catastrophes.
+NUCLEAR_CATASTROPHES = {
+    "Windscale/Sellafield Accident": {
+        "country": "United Kingdom",
+        "INES": "4",
+        "date": datetime(1973, 1, 1),
+    },
+    "Leningrad 1st Accident": {
+        "country": "Soviet Union",
+        "INES": "4-5",
+        "date": datetime(1974, 4, 6),
+    },
+    "Leningrad 2nd Accident": {
+        "country": "Soviet Union",
+        "INES": "4-5",
+        "date": datetime(1975, 10, 1),
+    },
+    "Beloyarsk 1st Accident": {
+        "country": "Soviet Union",
+        "INES": "5",
+        "date": datetime(1977, 1, 1),
+    },
+    "Jaslovské Bohunice Accident": {
+        "country": "Czechoslovakia",
+        "INES": "4",
+        "date": datetime(1977, 2, 1),
+    },
+    "Beloyarsk 2nd Accident": {
+        "country": "Soviet Union",
+        "INES": "3-4",
+        "date": datetime(1978, 12, 31),
+    },
+    "Three Mile Island Accident": {
+        "country": "United States",
+        "INES": "5",
+        "date": datetime(1979, 3, 28),
+    },
+    "Saint-Laurent Accident": {
+        "country": "France",
+        "INES": "4",
+        "date": datetime(1980, 1, 1),
+    },
+    "Tschernobyl Accident": {
+        "country": "Soviet Union",
+        "INES": "5",
+        "date": datetime(1982, 9, 1),
+    },
+    "Buenos Aires Accident": {
+        "country": "Argentina",
+        "INES": "4",
+        "date": datetime(1983, 1, 1),
+    },
+    "Wladiwostok Accident": {
+        "country": "Soviet Union",
+        "INES": "5",
+        "date": datetime(1985, 8, 10),
+    },
+    "Gore Accident": {
+        "country": "United States",
+        "INES": "2-4",
+        "date": datetime(1986, 1, 6),
+    },
+    "Tschernobyl Disaster": {
+        "country": "Soviet Union",
+        "INES": "7",
+        "date": datetime(1986, 4, 26),
+    },
+    "Sewersk Accident": {
+        "country": "Russia",
+        "INES": "2-4",
+        "date": datetime(1993, 4, 6),
+    },
+    "Tōkai-mura Accident": {
+        "country": "Japan",
+        "INES": "4-5",
+        "date": datetime(1999, 9, 30),
+    },
+    "Fleurus Accident": {
+        "country": "Belgium",
+        "INES": "4",
+        "date": datetime(2006, 3, 11),
+    },
+    "Fukushima Disaster": {
+        "country": "Japan",
+        "INES": "7",
+        "date": datetime(2011, 3, 11),
+    },
+}
+
+# Loop through the NUCLEAR_CATASTROPHES dictionary
+for catastrophe_name, catastrophe_data in NUCLEAR_CATASTROPHES.items():
+    catastrophe_date = catastrophe_data["date"]
+    catastrophe_country = catastrophe_data["country"]
+    catastrophe_year = catastrophe_data["date"].year
+    catastrophe_ines = catastrophe_data["INES"]
+
+    catastrophe_text = f"{catastrophe_name}, {catastrophe_country} ({catastrophe_year}, INES: {catastrophe_ines})"
+
+    # Add a vertical line
+    fig.add_shape(
+        type="line",
+        x0=catastrophe_date,
+        x1=catastrophe_date,
+        y0=1,
+        y1=-1,
+        yref="y",
+        xref="x",
+        line=dict(color="black", width=1, dash="solid"),
+    )
+
+    # Add a hover text annotation
+    fig.add_trace(
+        go.Scatter(
+            x=[catastrophe_date],
+            y=[0],
+            mode="markers",
+            marker=dict(size=0, opacity=0, color="white"),
+            text=[catastrophe_text],
+            hovertemplate="%{text}<extra></extra>",
+            showlegend=False,
         )
     )
 
@@ -164,7 +328,7 @@ for country in countries:
         t1 += " Years"
         t1 += "<br>"
         # Bauzeit
-        t1 += "Construction Time: "
+        t1 += "Construction to Operation Time: "  # "Construction Time: "
         t1 += country_data["Bauzeit"].round(1).apply(str)
         t1 += " Years"
         t1 += "<br>"
@@ -214,12 +378,12 @@ for country in countries:
         t2 += country_data["Abschaltung"].apply(lambda x: x.strftime("%B %Y"))
         t2 += "<br>"
         # Alter
-        t2 += "Age at Decommissioning: "
+        t2 += "Operating Time at Decommissioning: "
         t2 += country_data["Betriebszeit"].round(1).apply(str)
         t2 += " Years"
         t2 += "<br>"
         # Bauzeit
-        t2 += "Construction Time: "
+        t2 += "Construction to Operation Time: "  # "Construction Time: "
         t2 += country_data["Bauzeit"].round(1).apply(str)
         t2 += " Years"
         t2 += "<br>"
@@ -245,20 +409,20 @@ for country in countries:
             )
         )
 
-# Custom legend
-for country, color in COUNTRY_COLORS.items():
+for country in countries:
     fig.add_trace(
         go.Scatter(
             x=[None, None],
             y=[None, None],
             mode="markers",
-            marker=dict(size=12, color=color),
+            marker=dict(size=12, color=COUNTRY_COLORS[country]),
             name=country,
             legendgroup=country,
             showlegend=True,
             hoverinfo="none"
         )
     )
+
 
 # Compute the regression line
 x_values = [date.toordinal() for date in data.loc[data["Status"] == "Stillgelegt", "Abschaltung"]]
@@ -291,9 +455,10 @@ y_max = math.ceil(data.loc[data["Status"] == "In Betrieb", "Betriebszeit"].max()
 fig.update_xaxes(range=[x_min, x_max])
 fig.update_yaxes(range=[y_min, y_max])
 
+
 fig.add_annotation(
-    text="Commissioning<br>Decommissioning",
-    x=datetime(YEAR_START, 1, 1),
+    text=f"Commissioning{14*'<br>'}Decommissioning",
+    x=datetime(YEAR_START+1, 1, 1),
     y=0,
     xref="x",
     yref="y",
@@ -332,8 +497,14 @@ fig.add_annotation(
 )
 
 
+oldest_year = min(
+    df.loc[df["Status"]=="In Betrieb", "Kommerzieller Betrieb"].min().year,
+    df.loc[df["Status"]=="Stillgelegt", "Abschaltung"].min().year
+    )
+
+add_txt = f" since {YEAR_START}" if YEAR_START > oldest_year else ""
 fig.update_layout(
-    title="Evolution of Nuclear Power Plants in Europe since 1990:<br>Commissioning and Decommissioning of Reactors",
+    title=f"Evolution of Nuclear Power Plants in Europe{add_txt}:<br>Commissioning and Decommissioning of Reactors",
     xaxis_title="Year of Commissioning or Decommissioning",
     yaxis_title="Current Operational Age or Age at Decommissioning",
     xaxis=dict(showgrid=True, gridwidth=1, gridcolor="rgba(128, 128, 128, 0.1)"),
@@ -342,11 +513,48 @@ fig.update_layout(
     paper_bgcolor="rgba(0, 0, 0, 0)",
     # hovermode="x unified",
     hoverlabel=dict(font=dict(size=12)),
-    font=dict(family="Roboto", color="black", size=12),  # global font settings
+    font=dict(family="Times New Roman", color="black", size=12),  # global font settings
     width=997,
     height=580,
-    legend=dict(tracegroupgap=4),
+    legend=dict(tracegroupgap=.5),
 )
+
+
+# %%
+
+# # Add a slider
+# slider_steps = []
+# for i in range(YEAR_START, YEAR_END+1):
+#     step = dict(
+#         method="restyle",
+#         args=[
+#             {"x": [
+#                 data.loc[(data["Status"]=="In Betrieb") & (data["Land"]==country) & (data["Kommerzieller Betrieb"] >= datetime(i, 1, 1)) & (data["Kommerzieller Betrieb"] <= datetime(YEAR_END, 12, 31)), "Kommerzieller Betrieb"]
+#                 if country in countries else None
+#                 for country in countries
+#             ] + [
+#                 data.loc[(data["Status"]=="Stillgelegt") & (data["Land"]==country) & (data["Abschaltung"] >= datetime(i, 1, 1)) & (data["Abschaltung"] <= datetime(YEAR_END, 12, 31)), "Abschaltung"]
+#                 if country in countries else None
+#                 for country in countries
+#             ]}
+#         ],
+#         label=str(i),
+#     )
+#     slider_steps.append(step)
+# sliders = [dict(
+#     active=0,
+#     currentvalue={"prefix": "Year: "},
+#     pad={"t": 50},
+#     steps=slider_steps
+# )]
+# fig.update_layout(sliders=sliders)
+
+
+
+
+# %%
+
+
 
 
 # Save the plot as an HTML file
